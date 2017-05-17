e404c0eb76dd37da1c6412663ecfa3f4e617dc81
William Emfinger <waemfinger@gmail.com>
2017-05-17 13:03:31 -0700

    trying to get something working that will perform the proper handshake and get input data from the joycon.

--- scripts/joycon-client.py
+++ scripts/joycon-client.py
@@ -1,7 +1,18 @@
 import bluetooth
 import sys
 
+def hexString(s, delim=' '):
+    return delim.join('{:02x}'.format(ord(x)) for x in s)
+
 sock=bluetooth.BluetoothSocket(bluetooth.L2CAP)
+
+'''
+98:B6:E9:8B:78:B9	Joy-Con (L)
+7C:BB:8A:B8:8A:43	Joy-Con (R)
+'''
+
+jcL = "98:B6:E9:8B:78:B9"
+jcR = "7C:BB:8A:B8:8A:43"
 
 bd_addr = "7C:BB:8A:B8:8A:43"
 
@@ -21,9 +32,20 @@ sock.connect((bd_addr, port))
 
 print 'Connected'
 
-sock.send("\x08\x00\x92\x00\x01\x1F")
+commands = [
+    "\x80\x01\x00\x01\x1F",
+    "\x80\x02\x00\x01\x1F",
+    "\x80\x03\x00\x01\x1F",
+    "\x80\x91\x01\x00\x00\x00",
+    "\x08\x00\x92\x00\x01\x1F",
+    "\x08\x00\x92\x00\x01\x1F",
+    "\x08\x00\x92\x00\x01\x1F",
+    ]
 
-data = sock.recv(1024)
-print "received : " + ' '.join('{:02x}'.format(ord(x)) for x in data)
+for command in commands:
+    print "\nsent : " + hexString(command)
+    sock.send(command)
+    data = sock.recv(1024)
+    print "received : " + hexString(data)
 
 sock.close()
