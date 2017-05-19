c6277f3f3dc46f2cbed61fea72afbf27bfe413f7
William Emfinger <waemfinger@gmail.com>
2017-05-19 12:06:06 -0700

    working on program interface.

--- scripts/joycon-client.py
+++ scripts/joycon-client.py
@@ -1,4 +1,5 @@
 import bluetooth
+import time
 import sys
 
 def hexString(s, delim=' '):
@@ -14,7 +15,11 @@ sock=bluetooth.BluetoothSocket(bluetooth.L2CAP)
 jcL = "98:B6:E9:8B:78:B9"
 jcR = "7C:BB:8A:B8:8A:43"
 
-bd_addr = jcR
+if sys.argv[1] and sys.argv[1] == 'left':
+    bd_addr = jcL
+else:
+    bd_addr = jcR
+
 port = 1
 
 '''
@@ -31,18 +36,19 @@ for sm in service_matches:
 print "Got port: {}".format(port)
 '''
 
+print 'Connecting to {}'.format(bd_addr)
 sock.connect((bd_addr, port))
-
-print 'Connected'
+print 'Connected to  {}'.format(bd_addr)
 
 commands = [
     # service search attribute request
     "\x06\x00\x00\x00\x0d\x35\x03\x19\x11\x24\x00\x0f\x35\x03\x09\x02\x02\x00",
     # testing
-    "\x80\x01\xA2\x12\x00\x01\x1F",
-    "\x80\x01\xA2\x15\x00\x01\x1F",
 ]
 '''
+    "\x80\x01\xA2\x12\x00\x01\x1F",
+    "\x80\x01\xA2\x15\x00\x01\x1F",
+
     "\x80\x92\x00\x01\x00\x00\x00\x00\x1F",
     "\x80\x01\x00\x01\x1F",
     "\x80\x02\x00\x01\x1F",
@@ -55,9 +61,15 @@ commands = [
 '''
 
 for command in commands:
-    print "\nsent : " + hexString(command)
+    print ""
+    print "sent : {}".format(hexString(command))
     sock.send(command)
     data = sock.recv(1024)
-    print "received : " + hexString(data)
+    print "received : {}".format(hexString(data))
 
+time.sleep(5)
+
+print ""
+print 'Disconnecting from {}'.format(bd_addr)
 sock.close()
+print 'Disconnected from  {}'.format(bd_addr)
